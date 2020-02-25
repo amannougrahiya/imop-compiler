b219bbadef5bfb2d01fb8e1a09cc6ae80c1ee8e7
Aman Nougrahiya <aman.nougrahiya@gmail.com>
2020-02-25 10:13:50 -0800

    Updated README.

--- src/cgo20/demo1/Demo1.java
+++ src/cgo20/demo1/Demo1.java
@@ -8,6 +8,7 @@
  */
 package cgo20.demo1;
 
+import imop.ast.node.external.FunctionDefinition;
 import imop.ast.node.external.WhileStatement;
 import imop.lib.util.DumpSnapshot;
 import imop.lib.util.Misc;
@@ -24,17 +25,20 @@ public class Demo1 {
 		 * (A) Invoke the correct function here for parsing the file provided
 		 * via the parameter args.
 		 */
-		// TODO: Code here for (A).
+		Program.parseNormalizeInput(args);
 
 		/*
 		 * (B) From the generated AST, print the program to a file/terminal.
 		 */
+		System.out.println(Program.getRoot());
 		// TODO: Code here for (B)
 
 		/*
 		 * (C) Find and print all those statements that have a given label, say "test".
 		 */
-		// TODO: Code here for (C)
+		for (FunctionDefinition func : Program.getRoot().getInfo().getAllFunctionDefinitions()) {
+			System.out.println(func.getInfo().getStatementWithLabel("test"));
+		}
 
 		/*
 		 * (D) Invoke loop unrolling on the given while-statement, and print.
@@ -48,6 +52,7 @@ public class Demo1 {
 	}
 
 	public static void demo1d(WhileStatement whileStmt) {
+		whileStmt.getInfo().unrollLoop(2);
 		// TODO: Code here for (D)
 	}
 
